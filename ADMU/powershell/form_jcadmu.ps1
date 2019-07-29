de23ef3c24dae0794c743be67dffdd00c9b18b9a
Elliott Panipinto <elliott.panipinto@jumpcloud.com>
2019-07-29 11:13:26 -0600

    ElliottReview

--- ADMU/powershell/form_jcadmu.ps1
+++ ADMU/powershell/form_jcadmu.ps1
@@ -1,14 +1,21 @@
-$Invocation = (Get-Variable MyInvocation -Scope 0).Value
-$scriptPath = Split-Path $Invocation.MyCommand.Path
-. $scriptPath'\import_functions.ps1'
-
-$formpath = $scriptPath + '\xaml.ps1'
-$jcadmupath = $scriptPath + '\jcadmu.ps1'
-
-$FormResults = & $formpath
-If ($FormResults) {
-  $FormResults | Invoke-Expression -Command:($jcadmupath)
+# Get script path
+$scriptPath = (Split-Path -Path:($MyInvocation.MyCommand.Path)) + '\'
+# Define misc. variables
+$Template_Command = '& "{0}"'
+# Define ref files
+$functionsPath = $Template_Command -f ($scriptPath + 'import_functions.ps1')
+$xamlPath = $Template_Command -f ($scriptPath + 'xaml.ps1')
+$jcAdmuPath = $Template_Command -f ($scriptPath + 'jcadmu.ps1')
+# Load functions
+Invoke-Expression -Command:($functionsPath)
+# Load form
+$formResults = Invoke-Expression -Command:($xamlPath)
+# Send form results to process
+If ($formResults)
+{
+    $formResults | Invoke-Expression -Command:($jcAdmuPath)
 }
-Else {
-  Write-Error ('Form did not return anything.')
+Else
+{
+    Write-Error ('Form did not return anything.')
 }
