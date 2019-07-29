2561090aee88f05ff5a8c96b38fef4c8b7438fe7
btstevens <brad.stevens@gmail.com>
2019-07-29 14:09:56 -0600

    Functions not import_functions

--- ADMU/powershell/form_jcadmu.ps1
+++ ADMU/powershell/form_jcadmu.ps1
@@ -1,6 +1,6 @@
 $Invocation = (Get-Variable MyInvocation -Scope 0).Value
 $scriptPath = Split-Path $Invocation.MyCommand.Path
-. $scriptPath'\import_functions.ps1'
+. $scriptPath'\Functions.ps1'
 
 $formpath = $scriptPath + '\xaml.ps1'
 $jcadmupath = $scriptPath + '\jcadmu.ps1'
