c50b9aefa9febf4c70a86ce47e6da69a41b1ed88
Elliott Panipinto <elliott.panipinto@jumpcloud.com>
2019-07-30 11:07:38 -0600

    update error catching

--- ADMU/powershell/Start-JCADMU.ps1
+++ ADMU/powershell/Start-JCADMU.ps1
@@ -3,11 +3,11 @@ $scriptPath = (Split-Path -Path:($MyInvocation.MyCommand.Path))
 # Load form
 $formResults = Invoke-Expression -Command:('& "' + $scriptPath + '\Form.ps1"')
 # Send form results to process
-If ($formResults)
+If (-not [System.String]::IsNullOrEmpty($formResults))
 {
     & ($scriptPath + '\Migration.ps1') -inputObject:($formResults)
 }
 Else
 {
-    Write-Error ('Form did not return anything.')
+    Write-Host ('Exiting ADMU process')
 }
