732ef0651c014c68fd3c6e771a0b80ca1f182107
btstevens <brad.stevens@gmail.com>
2019-12-03 13:22:09 -0700

    TEST1

--- ADMU/powershell/Tests/TestSetup.ps1
+++ ADMU/powershell/Tests/TestSetup.ps1
@@ -44,4 +44,4 @@ if(Check_Program_Installed('Microsoft Visual C\+\+ 2013 x64') -or (Check_Program
 }
 
 #install jcagent and prereq
-    $ConfirmInstall = DownloadAndInstallAgent -msvc2013x64link:($msvc2013x64Link) -msvc2013path:($jcAdmuTempPath) -msvc2013x64file:($msvc2013x64File) -msvc2013x64install:($msvc2013x64Install) -msvc2013x86link:($msvc2013x86Link) -msvc2013x86file:($msvc2013x86File) -msvc2013x86install:($msvc2013x86Install)
+    #$ConfirmInstall = DownloadAndInstallAgent -msvc2013x64link:($msvc2013x64Link) -msvc2013path:($jcAdmuTempPath) -msvc2013x64file:($msvc2013x64File) -msvc2013x64install:($msvc2013x64Install) -msvc2013x86link:($msvc2013x86Link) -msvc2013x86file:($msvc2013x86File) -msvc2013x86install:($msvc2013x86Install)
